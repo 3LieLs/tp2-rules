ab1c3f33406c18aa0f40e625d02ad94b9a62e0a7
3LieLs <3lielfernandesribeiro@gmail.com>
2024-07-14 17:02:29 -0300

    Aula 10/07

--- servidor.js
+++ servidor.js
@@ -2,26 +2,20 @@ const express = require('express')
 const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
+
 app.get('/', (request, response) => {
-    response.send(`<form method="post" action="/calc">
-        <input id="num1" type="number" placeholder="Número 1" style="">
-        <input id="num2" type="number" placeholder="Número 2">
-        <br>
-        <input id="num3" type="number" placeholder="Número 3">
+    x = '', posFinal = ''
 
-        <input id="calcular" type="submit" value="Calcular">
-
-        <p id="ah">Resultado...</p>
-    </form>`)
+    response.render(`index`, {x, posFinal})
 })
 
-app.post('/calc', (request, response) => {
-    let resultado = num2.value * num3.value / num1.value
-    ah.innerText = (`${resultado}`)
-    console.log(`${resultado}`)
+app.post('/formulario', (request, response) => {
+    x = request.body.num2 * request.body.num3 / request.body.num1
+    posFinal = parseFloat(request.body.posInicial) + request.body.veloc * request.body.interv
 
-    response.render(`/`)
+    response.render(`index`)
 })
+
 const porta = 8080
 app.listen(porta, () => {
     console.log(`Servidor rodando em localhost:${porta}`)
