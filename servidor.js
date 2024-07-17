f4004668be680ac8e43fc2fb3a95dd90a700a766
Eliel Fernandes <3lielfernandesribeiro@gmail.com>
2024-07-17 12:10:18 -0300

    Aula 17/07

--- servidor.js
+++ servidor.js
@@ -9,6 +9,33 @@ app.get('/', (request, response) => {
     response.render(`index`, {x, posFinal})
 })
 
+app.post('/front', (request, response) => {
+    resultadoTotal = ''
+
+    response.render(`conta`, {resultadoTotal})
+})
+
+app.post('/resultSoma', (request, response) => {
+    resultadoTotal = parseFloat(request.body.n1) + parseFloat(request.body.n2)
+    response.render(`conta`)
+})
+
+app.post('/resultSub', (request, response) => {
+    resultadoTotal = parseFloat(request.body.n1) - parseFloat(request.body.n2)
+    response.render(`conta`)
+})
+
+app.post('/resultMult', (request, response) => {
+    resultadoTotal = parseFloat(request.body.n1) * parseFloat(request.body.n2)
+    response.render(`conta`)
+})
+
+app.post('/resultDiv', (request, response) => {
+    resultadoTotal = parseFloat(request.body.n1) / parseFloat(request.body.n2)
+    response.render(`conta`)
+})
+
+
 app.post('/formulario', (request, response) => {
     x = request.body.num2 * request.body.num3 / request.body.num1
     posFinal = parseFloat(request.body.posInicial) + request.body.veloc * request.body.interv
