c89c72f191cc8e4a808bc22fa2acc6cf8f7613d0
3LieLs <3lielfernandesribeiro@gmail.com>
2024-08-19 19:05:39 -0300

    19/08 Monitoria

---
+++ [MONITORIA]-Outros-servidores/servidor.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }))
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render("index", { nomeUsuario: "", resultado: "" });
+})
+
+app.get('/loja', (req, res) => {
+    res.render("loja", { compras: ""});
+})
+
+app
+    .route("/ID:leite")
+    .get((req, res) => {
+        res.render("comprarLoja");
+        res.redirect("/")
+    })
+
+app
+    .route("/teste")
+    .get((req, res) => {
+        res.redirect("/")
+    })
+    .post((req, res) => {
+        nomeUsuario = `Bem vindo(a): ${req.body.nomeUsuario}`
+        resultado = `Soma = ${parseFloat(req.body.n1) + parseFloat(req.body.n2)}`
+
+        res.render("index");
+    })
+
+const porta = 8080
+app.listen(porta, () => {
+    console.log(`Servidor rodando em localhost:${porta}`)
+})
